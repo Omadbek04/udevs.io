a2a07df7d9ae2ceb3855dbc6ba32e36bd9f1322f
omadbek <abdullayevomadbek709@gmail.com>
2024-07-14 14:17:38 +0500

    team page togirlandi

--- src/components/Command/Command.jsx
+++ src/components/Command/Command.jsx
@@ -23,7 +23,7 @@ const Command = () => {
     <section id="team" className="bg-navbarClose py-20">
       <div className="custom_contianer">
         <h1 className="text-[32px] md:text-[64px] text-start font-extrabold text-primary mb-[42px]">Team</h1>
-        <div className="flex items-center justify-between flex-col-reverse md:flex- gap-3 md:gap-0">
+        <div className="flex items-center justify-between flex-col-reverse md:flex-row gap-3 md:gap-0">
           <div className="flex-1">
             <p className="max-w-[500px] mb-[30px] text-[20px]  font-medium text-secondary leading-10">For each project, we form a team that includes a project manager, business analyst, UI / UX designer, DevOps, QA engineer, backend and front-end developers.</p>
             <div className="flex items-center text-primary text-[80px] md:text-[120px] mb-4 font-bold leading-0 md:leading-[70px]">
